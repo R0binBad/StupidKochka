60d56af9fbc8ab9cdf4df8639adcaf871ccc302c
R0binBad <Lappo.pavel@mail.ru>
2017-04-01 17:33:24 +0300

    #2 - Added kochka,jump button

---
+++ StupidKochka/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rb2d;
+    public float jumpPower;
+    public bool groundCheck;
+   
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+         groundCheck = true;
+    }
+
+   
+     public void Jump(bool isJump)
+    {
+        isJump = groundCheck;
+
+        if (groundCheck)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
+        }
+
+    }
+
+    
+
+
+}
+
