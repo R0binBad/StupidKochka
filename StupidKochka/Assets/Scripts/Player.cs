510d2312d3411efa96c850e310e2a12fd2d34d9d
R0binBad <Lappo.pavel@mail.ru>
2017-04-02 14:04:46 +0300

    #4 - Edit jump button, correct backgrounds layers,added barrier objects.

--- StupidKochka/Assets/Scripts/Player.cs
+++ StupidKochka/Assets/Scripts/Player.cs
@@ -8,21 +8,29 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb2d;
     public float jumpPower;
     public bool groundCheck;
-   
+    private Animator animator;
+    private SpriteRenderer sprite;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-
+       animator = GetComponent<Animator>();
+       sprite = GetComponent<SpriteRenderer>();
     }
-
 
     void Update()
     {
-         groundCheck = true;
+        if (groundCheck) State = CharState.Run;
+        CheckGround();
     }
 
-   
-     public void Jump(bool isJump)
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+
+    public void Jump(bool isJump)
     {
         isJump = groundCheck;
 
@@ -30,11 +38,25 @@ public class Player : MonoBehaviour
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
         }
-
     }
 
-    
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 2F);
 
+        groundCheck = colliders.Length > 1;
 
+        if (!groundCheck) State = CharState.Jump;
+    }
+
+   
 }
+
+ public enum CharState
+    {
+        Run,
+        Jump,
+        Sit
+    }
+
 
